c5572211f38d98de30befec8b460fc406ba58199
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-01-22 14:15:39 -0800

    fix: update OAuth Scope

--- retriever/settings.py
+++ retriever/settings.py
@@ -18,7 +18,8 @@ OAUTH2_PROVIDER_TOKEN_URL = os.getenv('OAUTH2_PROVIDER_TOKEN_URL')
 OAUTH2_CLIENT_ID = os.getenv('OAUTH2_CLIENT_ID')
 OAUTH2_CLIENT_SECRET = os.getenv('OAUTH2_CLIENT_SECRET')
 OAUTH2_REDIRECT_URI = os.getenv('OAUTH2_REDIRECT_URI')
-OAUTH2_SCOPE = os.getenv('OAUTH2_SCOPE')
+# OAUTH2_SCOPE = os.getenv('OAUTH2_SCOPE')
+OAUTH2_SCOPE = ['datahub:dataexports:download,read', 'datasets:bds:read', 'reporting:dataset:fetch,list', 'reporting:job:create,download,fetch,list']
 
 # SMB related environment variables
 NETWORK_DRIVE_USERNAME = os.getenv('NETWORK_DRIVE_USERNAME')
@@ -210,5 +211,6 @@ LOGGING = {
         }
     },
 }
+
 
 
