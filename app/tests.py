f61e296bdbcd817c4de015a8b3bf51d86b3f4fea
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-01-23 11:57:26 -0800

    add initial test case

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from app.tasks import cleanup
 
 # Create your tests here.
+def test_cleanup():
+    result = cleanup()
+    expected_result = None
+    assert result == expected_result
