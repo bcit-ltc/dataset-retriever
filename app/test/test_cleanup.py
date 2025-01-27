25b8f7f63225935ce74af2e8a0536e9646741fc2
Arvin Rolos <arolos@bcit.ca>
2025-01-27 09:54:19 -0800

    fix test

--- app/test/test_cleanup.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from app.tasks import cleanup
-
-# Create your tests here.
-def test_cleanup():
-    arg = "some_argument"
-    result = cleanup(arg)
-    expected_result = None
-    assert result == expected_result
---
+++ app/test/test_retriever.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from app.tasks import retriever
+
+class RetrieverTestCase(TestCase):
+    def test_retriever(self):
+        arg = "some_argument"
+        result = retriever(arg)
+        expected_result = None
+        self.assertEqual(result, expected_result)
