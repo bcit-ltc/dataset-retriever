17ba74fe1ed4994105a8f1da78bc7ce0a447e18d
Arvin Rolos <arolos@bcit.ca>
2025-01-27 10:05:20 -0800

    test

--- app/test/test_retriever.py
+++ app/test/test_retriever.py
@@ -5,5 +5,6 @@ class RetrieverTestCase(TestCase):
     def test_retriever(self):
         arg = "some_argument"
         result = retriever(arg)
+        print(f"Retriever result: {result}")
         expected_result = None
         self.assertEqual(result, expected_result)
