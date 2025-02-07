c46db17a65ee3974cac072999e457edb83495337
Arvin Rolos <arolos@bcit.ca>
2025-02-07 13:19:38 -0800

    fix testing 4

--- task_functions/test/test_retriever.py
+++ task_functions/test/test_retriever.py
@@ -15,8 +15,7 @@ class RetrieverTestCase(TestCase):
     @patch('task_functions.tasks.filter_objects_task.apply_async', return_value=MagicMock())
     @patch('task_functions.tasks.process_datasets_task.apply_async', return_value=MagicMock())
     @patch('task_functions.tasks.download_and_extract_files_task.apply_async', return_value=MagicMock())
-    @patch('task_functions.tasks.app.send_task', return_value=MagicMock())
-    def test_execute_sequential_tasks_success(self, mock_send_task, mock_download_and_extract_files, mock_process_datasets, mock_filter_objects, mock_fetch_datahub_data, mock_cache_get):
+    def test_execute_sequential_tasks_success(self, mock_download_and_extract_files, mock_process_datasets, mock_filter_objects, mock_fetch_datahub_data, mock_cache_get):
         # Mock the return values
         mock_fetch_datahub_data.return_value = {'Objects': [{'Full': {'Name': 'Role Details', 'ExtractsLink': 'http://example.com'}}]}
         mock_filter_objects.return_value = [{'Full': {'Name': 'Role Details', 'ExtractsLink': 'http://example.com'}}]
@@ -33,21 +32,17 @@ class RetrieverTestCase(TestCase):
         mock_process_datasets.assert_called_once()
         mock_download_and_extract_files.assert_called_once()
         mock_cache_get.assert_called_once()
-        mock_send_task.assert_called()
 
     @patch('task_functions.tasks.cache.get', return_value='mock_access_token')
-    @patch('task_functions.tasks.app.send_task', return_value=MagicMock())
-    def test_execute_sequential_tasks_connection_error(self, mock_send_task, mock_cache_get):
+    def test_execute_sequential_tasks_connection_error(self, mock_cache_get):
         # Call the task
         result = execute_sequential_tasks('some_argument')
         # Assertions
         self.assertIsNone(result)
-        mock_send_task.assert_called()
 
     @patch('task_functions.tasks.cache.get', return_value='mock_access_token')
     @patch('task_functions.tasks.fetch_datahub_data_task.apply_async', return_value=MagicMock())
-    @patch('task_functions.tasks.app.send_task', return_value=MagicMock())
-    def test_execute_sequential_tasks_fetch_datahub_data_failure(self, mock_send_task, mock_fetch_datahub_data, mock_cache_get):
+    def test_execute_sequential_tasks_fetch_datahub_data_failure(self, mock_fetch_datahub_data, mock_cache_get):
         # Mock the return values
         mock_fetch_datahub_data.return_value = None
 
@@ -58,4 +53,3 @@ class RetrieverTestCase(TestCase):
         self.assertIsNone(result)
         mock_fetch_datahub_data.assert_called_once()
         mock_cache_get.assert_called_once()
-        mock_send_task.assert_called()
