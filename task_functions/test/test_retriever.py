39eb4d6dccd5b8a61f606331c2ac19712d0cb0c3
Arvin Rolos <arolos@bcit.ca>
2025-02-07 15:11:02 -0800

    test

--- task_functions/test/test_retriever.py
+++ task_functions/test/test_retriever.py
@@ -1,42 +1,38 @@
 from django.test import TestCase
 from unittest.mock import patch, call
-from task_functions.tasks import execute_sequential_tasks
+from task_functions.tasks import execute_sequential_tasks, renew_token, fetch_datahub_data_task, filter_objects_task, process_datasets_task, download_and_extract_files_task, handle_task_failure
 
 class ExecuteSequentialTasksTest(TestCase):
 
-    @patch('task_functions.tasks.renew_token.s')
-    @patch('task_functions.tasks.fetch_datahub_data_task.s')
-    @patch('task_functions.tasks.filter_objects_task.s')
-    @patch('task_functions.tasks.process_datasets_task.s')
     @patch('task_functions.tasks.download_and_extract_files_task.s')
+    @patch('task_functions.tasks.process_datasets_task.s')
+    @patch('task_functions.tasks.filter_objects_task.s')
+    @patch('task_functions.tasks.fetch_datahub_data_task.s')
+    @patch('task_functions.tasks.renew_token.s')
     @patch('task_functions.tasks.chain')
-    @patch('django.core.cache.cache.get')
-    def test_execute_sequential_tasks(self, mock_cache_get, mock_chain, mock_download, mock_process, mock_filter, mock_fetch, mock_renew):
-        # Arrange
+    def test_execute_sequential_tasks(self, mock_chain, mock_renew_token, mock_fetch_datahub_data_task, mock_filter_objects_task, mock_process_datasets_task, mock_download_and_extract_files_task):
         arg = 20
-        mock_chain.return_value = mock_chain
-        mock_renew.return_value = None
-        mock_cache_get.return_value = "access_token"
-        mock_fetch.return_value = {'Objects': [{'Full': {'Name': 'Role Details', 'ExtractsLink': 'http://example.com'}}]}
-        mock_filter.return_value = [{'Full': {'Name': 'Role Details', 'ExtractsLink': 'http://example.com'}}]
-        mock_process.return_value = [{'Name': 'RoleDetails', 'ExtractsLink': 'http://example.com'}]
-        mock_download.return_value = None
+        filter_names = [
+            "Role Details",
+            "Users",
+            "Organizational Units",
+            "Enrollments and Withdrawals",
+        ]
 
-        # Act
         execute_sequential_tasks(arg)
-        
-        # Assert
-        mock_renew.assert_called_once_with(arg)
-        mock_cache_get.assert_called_once_with('ACCESS_TOKEN')
-        mock_fetch.assert_called_once_with("access_token")
-        mock_filter.assert_called_once_with(mock_fetch.return_value, ['Role Details', 'Users', 'Organizational Units', 'Enrollments and Withdrawals'], 'Full')
-        mock_process.assert_called_once_with(mock_filter.return_value, 'Full')
-        mock_download.assert_called_once_with(mock_process.return_value)
+
+        mock_renew_token.assert_called_once_with(arg)
+        mock_fetch_datahub_data_task.assert_called_once_with()
+        mock_filter_objects_task.assert_called_once_with(filter_names, 'Full')
+        mock_process_datasets_task.assert_called_once_with('Full')
+        mock_download_and_extract_files_task.assert_called_once_with()
+
         mock_chain.assert_called_once_with(
-            mock_renew.return_value,
-            mock_fetch.return_value,
-            mock_filter.return_value,
-            mock_process.return_value,
-            mock_download.return_value
+            mock_renew_token(),
+            mock_fetch_datahub_data_task(),
+            mock_filter_objects_task(),
+            mock_process_datasets_task(),
+            mock_download_and_extract_files_task()
         )
-        mock_chain().apply_async.assert_called_once_with(link_error=mock_chain().link_error)
+
+        mock_chain().apply_async.assert_called_once_with(link_error=handle_task_failure.s())
